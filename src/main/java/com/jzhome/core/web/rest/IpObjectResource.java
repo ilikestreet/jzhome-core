2d18d938d08ccbca999622b8ca0367a020792109
ilikestreet <ilikestreet.jz@gmail.com>
2018-11-19 23:10:41 +0800

    trigger build

--- src/main/java/com/jzhome/core/web/rest/IpObjectResource.java
+++ src/main/java/com/jzhome/core/web/rest/IpObjectResource.java
@@ -24,6 +24,7 @@ public class IpObjectResource {
 
     private final Logger log = LoggerFactory.getLogger(IpObjectResource.class);
 
+
     @Autowired
     private IpObjectService ipObjectService;
 
